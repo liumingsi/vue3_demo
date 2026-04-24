185368f905f7374ed375de346a9730b7a1feef8b
liusiming <liusiming@bi4sight.onaliyun.com>
2026-04-24 16:24:10 +0800

    vite

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,17 @@
 import { fileURLToPath, URL } from 'node:url'
 
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
-
+const env = loadEnv(mode, process.cwd())
 // https://vite.dev/config/
 export default defineConfig({
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+  base: env.VITE_BASE_PATH || '/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
